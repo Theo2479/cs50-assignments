45ab517732a570af792716e7dd5ff94f9bc49fdc
Theo <Theo@mbp-theo-jouas.home>
2025-06-11 17:11:39 +0100

    add

---
+++ faces.py
@@ -0,0 +1,12 @@
+# Get input from the user, replace faces with emojis, and print the result
+def convert(text):
+    text = text.replace(":)", "🙂")
+    text = text.replace(":(", "🙁")
+    return text
+
+def main():
+    user_input = input("")
+    print(convert(user_input))
+
+main()
+
